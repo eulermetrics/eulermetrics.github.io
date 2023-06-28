59513bed7b38347a0ccb1f43a5f8d0c9f2c4d209
Alex <mogollonwilmer@gmail.com>
2023-06-28 17:43:05 -0400

    add email

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_ebt42lx";
+    const templateID = "template_g1q1k9e";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
